0d7da256e11c40f6d7d57ae869f71f3e5466e760
swarnavs <swarnav@capitalnumbers.com>
2025-04-10 10:43:21 +0530

    Change image base url

--- src/environments/prod.env.ts
+++ src/environments/prod.env.ts
@@ -4,5 +4,5 @@ export const ProdEnvironment: Environment = {
   db_url:
     "mongodb+srv://swarnav:Swarnav294@cluster0.zl39t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   jwt_secret: "prodSecret",
-  base_url: "http://localhost:5000/",
+  base_url: "https://dynamic-blog-backend.onrender.com/",
 };
